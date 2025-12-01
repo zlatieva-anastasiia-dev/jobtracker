46aa73a81a3c8cca1428ada7949be1ce09b58efb
zlatieva-anastasiia-dev <159052978+zlatieva-anastasiia-dev@users.noreply.github.com>
2025-12-01 16:32:13 +0100

    feat:shared Button component

---
+++ components/ui/Button.tsx
@@ -0,0 +1,24 @@
+import { ButtonHTMLAttributes } from "react";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "secondary" | "danger";
+};
+
+export function Button({
+  variant = "primary",
+  children,
+  ...props
+}: ButtonProps) {
+  const variantStyles = {
+    primary: `bg-blue-500 hover:bg-blue-600 text-white `,
+    secondary: "bg-gray-500 hover:bg-gray-600 text-white",
+    danger: "bg-red-500 hover:bg-red-600 text-white",
+  };
+  const base = `px-4 py-2 rounded-md transition-colors`;
+
+  return (
+    <button className={`${base} ${variantStyles[variant]}`} {...props}>
+      {children}
+    </button>
+  );
+}
